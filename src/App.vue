7b19446e90b6db196d1b8ca05d7afd01f9f62e1b
pontakornth <pontakorn61@gmail.com>
2021-06-11 15:10:14 +0700

    refactor: Make it data

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,109 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+<main>
+  <div class="tabs">
+    <div class="tab active">All</div>
+    <div class="tab">Active</div>
+    <div class="tab">Completed</div>
+  </div>
+  <div class="todo-list">
+    <div class="todo-menu">
+      <input class="todo-input" name="todo-title" />
+      <button class="primary">Add Todo</button>
+    </div>
+    <todo-item v-for="todo in todos" :key="todo.id" :todo="todo" />
+  </div>
+</main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import TodoItem from "./components/TodoItem.vue"
 
 export default defineComponent({
   name: 'App',
+  data() {
+    return {
+      todos:  [
+        {
+          task: "Fighting gold",
+          isComplete: false,
+          id: "reiktirkit"
+        },
+        {
+          task: "Find traitor requiem",
+          isComplete: true,
+          id: "eiluriedkiki"
+        }
+      ]
+    }
+  },
+  methods: {},
   components: {
-    HelloWorld
+    TodoItem
   }
 })
 </script>
 
 <style>
-#app {
+* {
+  box-sizing: border-box;
+}
+main {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  min-width: 80%;
+  max-width: 90%;
+  margin: auto;
+  padding: 2rem;
+}
+.primary {
+  color: white;
+  background: blue;
+  border: none;
+  cursor: pointer;
+}
+.accent {
+  color: white;
+  background: red;
+  border: none;
+  cursor: pointer;
+}
+.tabs {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  text-align: center;
+  cursor: pointer;
+}
+.tab {
+  padding: .55rem;
+  background: white;
+  text-decoration: underline;
+  color: blue;
+  border: 1px solid blue;
+}
+.tab.active {
+  color: white;
+  background: blue;
+}
+.todo-input {
+  flex: 1;
+}
+.todo-list {
+  padding: 1rem;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: space-evenly;
+  align-items: center;
+  border: 1px solid blue;
+}
+.todo-menu {
+  display: flex;
+  justify-content: space-between;
+  flex-flow: row nowrap;
+  width: 100%;
 }
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="todo-item">
+    <div class="todo-content">
+      <input class="todo-checkbox" type="checkbox" name="todo"/>
+      {{todo.task}}
+    </div>
+    <div class="todo-action">
+      <button class="accent">Delete</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+
+type Todo = {
+  task: string,
+  isComplete: boolean,
+  id: string,
+}
+
+export default defineComponent({
+  name: "TodoItem",
+  props: {
+    todo: {
+      type: Object as PropType<Todo>,
+      required: true
+    }
+  }
+})
+</script>
+
+<style scoped>
+.todo-item {
+  padding: 1rem;
+  display: flex;
+  border: 1px solid blue;
+  width: 100%;
+  margin-top: 1rem;
+  justify-content: space-between;
+}
+.todo-content {
+  text-align: left;
+}
+</style>
